bf89bbb18de1f1ac0b1f34c73f4d200c56f79375
Alex Bazlinton <abazlinton@gmail.com>
2017-08-10 16:41:21 +0100

    test delayed

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 
-
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  setTimeout(res.send('Hello World!'), 1000);
+  
 })
 
-app.listen(process.env.PORT, function () {
+app.listen(app.get('port'), function () {
   console.log('Example app listening on port 80!')
 })
