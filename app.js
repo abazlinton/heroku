1c07ab5bbd72f644c18cf06003db8d808e8f868c
Alex Bazlinton <abazlinton@gmail.com>
2017-08-10 13:35:13 +0100

    get port properly - won't be 80 as probably forwarded

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-console.log(process.env.PORT);
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(80, function () {
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 80!')
 })
